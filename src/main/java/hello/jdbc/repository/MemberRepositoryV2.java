7c0e02d1d0253c6a8843d7e0f4e6d17a10d55b56
MinseokGo <rhalstjr1999@naver.com>
2024-04-13 17:23:15 +0900

    feat: member sevice v2

---
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV2.java
@@ -0,0 +1,121 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class MemberRepositoryV2 {
+
+    private final DataSource dataSource;
+
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id, money) values(?, ?)";
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, member.getMemberId());
+            statement.setInt(2, member.getMoney());
+            statement.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            log.error("Database error!!", e);
+            throw e;
+        }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, memberId);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return Member.valueOf(
+                            resultSet.getString("member_id"),
+                            resultSet.getInt("money"));
+                }
+            }
+            throw new NoSuchElementException("member not found memberId = " + memberId);
+        } catch (SQLException e) {
+            log.error("Database error!!", e);
+            throw e;
+        }
+    }
+
+    public Member findById(Connection connection, String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, memberId);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return Member.valueOf(
+                            resultSet.getString("member_id"),
+                            resultSet.getInt("money"));
+                }
+            }
+            throw new NoSuchElementException("member not found memberId = " + memberId);
+        } catch (SQLException e) {
+            log.error("Database error!!", e);
+            throw e;
+        }
+    }
+
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money=? where member_id=?";
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, money);
+            statement.setString(2, memberId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            log.info("Database error!!", e);
+            throw e;
+        }
+    }
+
+    public void update(Connection connection, String memberId, int money) throws SQLException {
+        String sql = "update member set money=? where member_id=?";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, money);
+            statement.setString(2, memberId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            log.info("Database error!!", e);
+            throw e;
+        }
+    }
+
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id=?";
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, memberId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        log.info("connection={} class={}", connection, connection.getClass());
+        return connection;
+    }
+}
---
+++ src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,60 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV2;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+
+    private final DataSource dataSource;
+    private final MemberRepositoryV2 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Connection connection = dataSource.getConnection();
+
+        try {
+            connection.setAutoCommit(false);
+            accountTransferBusinessLogic(connection, fromId, toId, money);
+            connection.commit();
+        } catch (Exception e) {
+            connection.rollback();
+            throw new IllegalStateException(e);
+        } finally {
+            release(connection);
+        }
+    }
+
+    private void accountTransferBusinessLogic(Connection connection, String fromId, String toId, int money)
+            throws SQLException {
+
+        Member fromMember = memberRepository.findById(connection, fromId);
+        Member toMember = memberRepository.findById(connection, toId);
+
+        memberRepository.update(connection, fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(connection, toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+
+    private void release(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.setAutoCommit(true);
+                connection.close();
+            } catch (Exception e) {
+                log.info("Error!!", e);
+            }
+        }
+    }
+}
